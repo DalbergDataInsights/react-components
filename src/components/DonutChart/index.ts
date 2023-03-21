5cc22b4cb28f15f1201aa7204542dd282ebe9c05
Rohianon <rohianon48@gmail.com>
2023-03-21 10:50:51 +0300

    add manifest to index.ts

--- src/components/DonutChart/index.ts
+++ src/components/DonutChart/index.ts
@@ -1,2 +1,11 @@
-export { default } from './donutchartComponent'
+import DonutChartComponent from './donutchartComponent'
+import { defaults } from './donutchartConfig'
+
+export const manifest = {
+  name: "DonutChart",
+  version: "0.1",
+  defaults: defaults,
+  Component: DonutChartComponent,
+}
+
 export type { iDonutChart } from './donutchartInterface'
