bfd25b04c28ac1d7c1d39fe310b01c04f93a9439
Rohianon <rohianon48@gmail.com>
2023-06-06 16:01:28 +0300

    add noPaddingGetter function, interface definition

--- src/hooks/useDim.tsx
+++ src/hooks/useDim.tsx
@@ -1,13 +1,23 @@
 import React, { useLayoutEffect, useRef, useState } from "react"
+import { useDimInterface } from "./useDimInterface"
 
-export const useDim = ({ getter = defaultGetter, on = "resize" }) => {
+export const useDim = ({ 
+  getter = defaultGetter, 
+  on = "resize", 
+  radiusOffset = 0 
+}: useDimInterface) => {
   const ref = useRef(null)
 
-  const [prop, setProp] = useState(0)
+  const [prop, setProp] = useState<any | null>(0)
+
+  const finalGetter = typeof getter === "function" 
+    ? getter : getter === "nopadding" 
+      ? noPaddingGetter(radiusOffset) 
+      : defaultGetter
 
   const handleWindowSizeChange = () => {
     try {
-      setProp(getter(ref.current))
+      setProp(finalGetter(ref.current))
     } catch {
       setProp(undefined)
     }
@@ -32,6 +42,32 @@ export const useDim = ({ getter = defaultGetter, on = "resize" }) => {
   return { ref, prop }
 }
 
-function defaultGetter(current: any) {
+const defaultGetter = (current: any) => {
   return { width: current.offsetWidth, height: current.offsetHeight }
+}
+
+const noPaddingGetter = (radiusOffset: number) => (current: any) => {
+  const { viewportElement } = current
+  const { parentElement: child } = viewportElement
+  let targetElement = child
+
+  while (targetElement) {
+    const { padding } = window.getComputedStyle(targetElement)
+    
+    if (!isNaN(parseFloat(padding)) && parseFloat(padding) > 0) {
+      break
+    }
+
+    targetElement = targetElement.parentElement
+  }
+
+  const { 
+    paddingTop, paddingBottom, paddingLeft, paddingRight 
+  } = window.getComputedStyle(targetElement)
+  const width = viewportElement.clientWidth - 
+    parseFloat(paddingLeft) - parseFloat(paddingRight)
+  const height = viewportElement.clientHeight - 
+    parseFloat(paddingTop) - parseFloat(paddingBottom)
+
+  return Math.min(width, height)/2 - radiusOffset
 }
