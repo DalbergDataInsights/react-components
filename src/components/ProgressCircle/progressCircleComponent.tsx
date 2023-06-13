8b2fda86d8abc5c7c7801ca7102e771116ca4946
Denys Sementsov <denys.sementsov@dalberg.com>
2023-06-13 21:32:09 +0000

    refactoring the progress circle component usedim

--- src/components/ProgressCircle/progressCircleComponent.tsx
+++ src/components/ProgressCircle/progressCircleComponent.tsx
@@ -17,12 +17,8 @@ export const ProgressCircleComponent = ({
     .fontSize.match(/\d+/g)
   const radiusOffset = rootFontSize ? Number(rootFontSize[0]) : 16
 
-  const { ref, prop: radius } = useDim({
-    getter: (c) =>
-      Math.min(c.viewportElement.clientWidth, c.viewportElement.clientHeight) /
-        2 -
-      radiusOffset,
-  })
+  const { ref, prop } = useDim({ getter: "client" })
+  const radius = Math.min(prop.width, prop.height) / 2 - radiusOffset
   const circumference = Math.round(radius * 2 * Math.PI)
   const [offset, setOffset] = useState("300%")
 
